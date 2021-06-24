00fce3b8c658026033aa686da6576657d21ca8f1
coderadu <47945947+coderadu@users.noreply.github.com>
2021-06-24 19:54:22 +0300

    Fixed security risk

--- src/components/storage/File.js
+++ src/components/storage/File.js
@@ -8,6 +8,7 @@ function FIle({ file }) {
       href={file.url}
       target="_blank"
       className="btn btn-outline-dark text-truncate w-100"
+      ref="noreferrer"
     >
       <FontAwesomeIcon icon={faFile} className="mr-2" /> {file.name}
     </a>
